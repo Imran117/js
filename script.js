d6d979d7f403fc364be04f99c48aca3b9dd334d2
Imron <umurzakov117ali@gmail.com>
2020-12-27 15:52:51 +0500

    finished first part

---
+++ script.js
@@ -0,0 +1,40 @@
+// console.log(1);
+// console.log('hello world!');
+// console.log(true);
+
+// var nikName = 'Imron';
+// console.log(nikName);
+
+// var age = 132;
+// console.log(age);
+
+
+// var boolean = true;
+// console.log(boolean);
+
+// var und;
+// console.log(und);
+
+// console.log('mening ismim ' + nikName +' mening yoshim ' + age + ' da');
+// var allInfo = 'mening ismim ' + nikName +' mening yoshim ' + age + ' da';
+
+// console.log(allInfo);
+
+// console.log(6 + 3);
+// console.log(6 - 3);
+// console.log(6 * 3);
+// console.log(6 / 3);
+// console.log(6 % 4);
+
+
+// console.log(true + true);
+
+// var nick = prompt('ismingizni kiriting')
+
+// console.log(nick);
+
+// alert('salom '+ nick);
+
+// var num = +prompt('yoshingizni kiriting')
+// console.log(num);
+
