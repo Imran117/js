a5c47a3e0c2ee5bff44ec3364f798922c64ef50c
Imron <umurzakov117ali@gmail.com>
2020-12-27 16:59:52 +0500

    finished js

--- script.js
+++ script.js
@@ -38,3 +38,67 @@
 // var num = +prompt('yoshingizni kiriting')
 // console.log(num);
 
+// console.log(1 == 1);
+// console.log(1 === '1');
+
+// console.log(1 < 1);
+// console.log(1 < 2);
+// console.log(1 > 2);
+// console.log(5 > 2);
+// console.log(5 > 2);
+
+// console.log(5 >= 5);
+// console.log(5 >= 6);
+// console.log(5 <= 6);
+// console.log(5 <= 5);
+
+// console.log(1 != 1);
+// console.log(1 != 2);
+// console.log(!true);
+
+// if (1 == 1) {
+//     console.log('if');
+// }else{
+//     console.log('else');
+// }
+
+
+// var a = prompt('salom so`zini kiriting!');
+
+// if (a == 'salom') {
+//     alert('hush kelibsiz')   
+// }else{
+//    alert('Noto`g`ri') 
+// }
+
+// var b = prompt('5 sonini kriting!!!')
+// if (b == 5) {
+//     console.log('to`g`ri');
+// }else if (b < 5) {
+//     console.log('5dan kichik');
+// }else if (b > 5) {
+//     console.log('5dan katta');
+// }else{
+//     console.log('bu son emas');
+// }
+
+// console.log(isNaN("sefsef"));
+
+var number1 = +prompt('brinchi sonni kiriting')
+var number2 = +prompt('ikkinchi sonni kiriting')
+
+if (number1 == number2 && !isNaN(number1) && !isNaN(number2)) {
+    console.log('sonlar teng');
+} else if (number1 < number2 && !isNaN(number1) && !isNaN(number2)) {
+    console.log('birinchi son kichkina' );
+} else if (number1 > number2  && !isNaN(number1) && !isNaN(number2)) {
+    console.log('ikkinchi son kichkina');
+} else {
+    if (isNaN(number1) && isNaN(number2)) {
+        console.log('Sonlar noto`g`ri');
+    } else if (isNaN(number1)) {
+        console.log('Brinchi son noto`g`ri');
+    } else if (isNaN(number)) {
+        console.log('ikkinchi son notog`ri');
+    }
+}
